a50589268a525ecfe0314135e5e8fedd3d53d196
Junior de Oliveira <ellismarjr@hotmail.com>
2021-04-26 10:08:17 -0300

    add Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
-export default function Home() {
+import { Button, Flex, FormControl, FormLabel, Stack } from '@chakra-ui/react';
+import { Input } from '../components/form/Input';
+
+export default function SignIn() {
   return (
-    <h1>DASHGO</h1>
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+        <Button
+          type="submit"
+          marginTop={6}
+          colorScheme="pink"
+          size="lg"
+        >Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
