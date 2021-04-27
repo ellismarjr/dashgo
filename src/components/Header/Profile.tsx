26833bc7929113cc457ecfe3ced9f313d172f157
Junior de Oliveira <ellismarjr@hotmail.com>
2021-04-27 15:49:36 -0300

    Sidebar responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,27 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Júnior de Oliveira</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          ellismarjr@gmail.com
+
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Júnior de Oliveira</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            ellismarjr@gmail.com
             </Text>
-      </Box>
+        </Box>
+      )}
 
       <Avatar
         size="md"
