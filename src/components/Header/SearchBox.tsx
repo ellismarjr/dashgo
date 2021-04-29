dd2e5dc839d43c68bd108b9b5889381efdadf598
Junior de Oliveira <ellismarjr@hotmail.com>
2021-04-29 09:04:34 -0300

    add signIn input data validation

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,8 +1,10 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <Flex
       as="label"
@@ -18,6 +20,7 @@ export function SearchBox() {
       borderRadius="full"
     >
       <Input
+        ref={searchInputRef}
         color="gray.50"
         variant="unstyled"
         placeholder="Buscar na plataforma"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
-import { Button, Flex, FormControl, FormLabel, Stack } from '@chakra-ui/react';
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from '../components/form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = Yup.object().shape({
+  email: Yup.string().required('E-mail é obrigatório').email('E-mail inválido'),
+  password: Yup.string().required('Senha é obrigatória')
+});
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values)
+  }
+
   return (
     <Flex
       width="100vw"
@@ -17,16 +40,30 @@ export default function SignIn() {
         padding="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={formState.errors.email}
+            {...register('email')}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            error={formState.errors.password}
+            {...register('password')}
+          />
         </Stack>
         <Button
           type="submit"
           marginTop={6}
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >Entrar</Button>
       </Flex>
     </Flex>
