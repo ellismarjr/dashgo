b1c7dba8625c9e203e887776fae252351f33046e
Junior de Oliveira <ellismarjr@hotmail.com>
2021-05-07 10:54:08 -0300

    using react query to fetching users data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,6 @@ import {
   useBreakpointValue
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from 'react-query';
 
@@ -24,37 +23,16 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebabr";
 
+import { useUsers } from "../../services/hooks/useUsers";
+
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json();
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users;
-  });
+  const { data, isLoading, error, isFetching } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
     md: true,
     lg: true
   });
-
-
-  useEffect(() => {
-
-  }, []);
 
   return (
     <Box>
@@ -65,7 +43,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex marginBottom="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
             <Link href="/users/create">
               <Button
